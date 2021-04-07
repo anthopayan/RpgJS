9eadf9cb5bc73fe4dfd67c3b9e1f1bc14aee2462
Neeiyo <louis.grandjean99@gmail.com>
2021-04-07 14:42:28 +0200

    game fix

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
 class Game {
+    constructor(turnLeft = 10) {
+        this.turnLeft = turnLeft;
+    }
 
+    newTurn = () => {
+        if (this.turnLeft > 0) {
+            this.turnLeft = this.turnLeft - 1;
+        } else {
+            console.log("partie finie");
+        }
+
+    }
 }
